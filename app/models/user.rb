d83cdc61a032dc932c26b0a17124cd351dd0294a
Wiwatta Mongkhonchit <zentetsukenz@gmail.com>
2015-04-11 00:05:24 +0700

    Add user model functionalities
    - Authenticate, for password authentication
    - hash password before create new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
+require 'scrypt'
+
 class User < ActiveRecord::Base
+
+  validates :username, :password, presence: true
+
+  before_create :hash_password
+
+  def authenticate(password)
+    SCrypt::Password.new(self.password) == password
+  end
+
+
+
+
+  private
+
+  def hash_password
+    self.password = SCrypt::Password.create(self.password)
+  end
+
 end
